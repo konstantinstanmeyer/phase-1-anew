bfd0c778a301d025442f7d1c02bc24c63cdfb142
KonstantinStanmeyer <konstantin@stanmeyer.com>
2026-01-19 09:08:06 -0500

    setting baseUrl

--- cocktailbackend/src/main/java/com/cocktailbackend/config/WebClientConfig.java
+++ cocktailbackend/src/main/java/com/cocktailbackend/config/WebClientConfig.java
@@ -1,9 +1,14 @@
 package com.cocktailapp.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+    
+    @Value("${cocktaildb.api.base-url}")
+    private String baseUrl;
     
 }
