49927b91296d0acec0ce19281b54838400e75b18
KonstantinStanmeyer <konstantin@stanmeyer.com>
2026-01-19 08:57:41 -0500

    empty initializer + basic response setup with DrinkResponse

---
+++ cocktailbackend/src/main/java/com/cocktailbackend/model/DrinkResponse.java
@@ -0,0 +1,13 @@
+package com.cocktailapp.model;
+
+import java.util.List;
+
+public class DrinkResponse {
+    // using list for binding structure/content
+    private List<Drink> drinks;
+
+
+    public DrinkResponse () {
+
+    }
+}
