50c6b3c568d31b78a2b70fb2d193894d617442f3
KonstantinStanmeyer <konstantin@stanmeyer.com>
2026-01-19 07:43:49 -0500

    uploading spring-boot backend

---
+++ backend/src/main/java/com/example/phase_1_anew/Phase1AnewApplication.java
@@ -0,0 +1,14 @@
+package com.example.phase_1_anew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase1AnewApplication {
+
+	// comment
+	public static void main(String[] args) {
+		SpringApplication.run(Phase1AnewApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/phase_1_anew/Phase1AnewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phase_1_anew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Phase1AnewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
