e908b75812d318539ceb2aee29a3a65d8392bcfe
KonstantinStanmeyer <konstantin@stanmeyer.com>
2026-01-19 08:41:50 -0500

    data fields visual for drink model setup

---
+++ cocktailbackend/src/main/java/com/cocktailbackend/model/Drink.java
@@ -0,0 +1,28 @@
+package com.cocktailapp.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// data fields
+// idDrink: "15346"
+// ingredients: (4) ['1 shot Dark rum', '2 shots Light rum', '1 shot Vodka', '1 shot Orange juice']
+// strDrink: "155 Belmont"
+// strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/yqvvqs1475667388.jpg"
+// strIngredient1: "Dark rum"
+// strIngredient2: "Light rum"
+// strIngredient3: "Vodka"
+// strIngredient4: "Orange juice"
+// strIngredient5: null
+// strIngredient6: null
+// strInstructions: "Blend with ice. Serve in a wine glass. Garnish with carrot."
+// strMeasure1: "1 shot "
+// strMeasure2: "2 shots "
+// strMeasure3: "1 shot "
+// strMeasure4: "1 shot "
+// strMeasure5: null
+// strMeasure6: null
+
+
+public class Drink {
+    
+}
