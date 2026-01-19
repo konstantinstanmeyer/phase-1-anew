93687b070f3180905d9a979c7afabd95481f633f
KonstantinStanmeyer <konstantin@stanmeyer.com>
2026-01-19 09:32:06 -0500

    built "preview" getDrinksByIngredients - pre-controller testing

--- cocktailbackend/src/main/java/com/cocktailbackend/service/DrinkService.java
+++ cocktailbackend/src/main/java/com/cocktailbackend/service/DrinkService.java
@@ -10,5 +10,29 @@ import java.util.List;
 
 @Service
 public class DrinkService {
+    private final WebClient webClient;
     
+    public DrinkService(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public List<Drink> getDrinksByIngredient(String ingredient) {
+        DrinkResponse response = webClient.get()
+                .uri("/filter.php?i=" + ingredient)
+                .retrieve()
+                .bodyToMono(DrinkResponse.class)
+                .block();
+        
+        // edge-case error handling for incorrent queries (presuming api endpoints will always return drinks with project's approach)
+        if (response == null || response.getDrinks() == null) {
+            return Collections.emptyList();
+        }
+        
+        // avoiding unnecessary bloat on initial, surface-level query
+        return response.getDrinks().stream()
+                .limit(5)
+                .map(drink -> getDrinkById(drink.getIdDrink()))
+                .filter(drink -> drink != null)
+                .toList();
+    }
 }
