38b166ec6b3465a519bcd1b56f7efba5cfc07d07
KonstantinStanmeyer <konstantin@stanmeyer.com>
2026-01-19 09:07:17 -0500

    basic imports for WebClientConfig for api accessing

---
+++ cocktailbackend/src/main/java/com/cocktailbackend/config/WebClientConfig.java
@@ -0,0 +1,9 @@
+package com.cocktailapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    
+}
