1aa0b531a2a9a7b9f2dcb137bc24691efb2bee50
KonstantinStanmeyer <konstantin@stanmeyer.com>
2026-01-19 09:33:56 -0500

    pre-postman testing w/ controller

---
+++ cocktailbackend/src/main/java/com/cocktailbackend/controller/DrinkController.java
@@ -0,0 +1,23 @@
+package com.cocktailapp.controller;
+
+import com.cocktailapp.model.Drink;
+import com.cocktailapp.service.DrinkService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/drinks")
+public class DrinkController {
+    
+    private final DrinkService drinkService;
+    
+    public DrinkController(DrinkService drinkService) {
+        this.drinkService = drinkService;
+    }
+    
+    @GetMapping("/by-ingredient")
+    public ResponseEntity<List<Drink>> getDrinksByIngredient(@RequestParam String ingredient) {
+        List<Drink> drinks = drinkService.getDrinksByIngredient(ingredient);
+        return ResponseEntity.ok(drinks);
+    }
+}
