c7d10e98a3289a3b4b08a30c9e14eab24043643f
KonstantinStanmeyer <konstantin@stanmeyer.com>
2026-01-19 08:40:06 -0500

    Initial project setup with Spring Boot

---
+++ cocktailbackend/src/main/java/com/cocktailbackend/CocktailbackendApplication.java
@@ -0,0 +1,13 @@
+package com.cocktailbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailbackendApplication.class, args);
+	}
+
+}
---
+++ cocktailbackend/src/test/java/com/cocktailbackend/CocktailbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocktailbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
