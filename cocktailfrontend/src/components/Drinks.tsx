e1f0d5e291cc2be2df1df90b4f7dba34cfa76273
KonstantinStanmeyer <konstantin@stanmeyer.com>
2026-01-20 07:02:43 -0500

    return back function for refetch

--- cocktailfrontend/src/components/Drinks.tsx
+++ cocktailfrontend/src/components/Drinks.tsx
@@ -1,17 +1,22 @@
 import { useState } from 'react';
 
-// interface Drink {
-
-// }
-
 const LIQUORS = [ "Vodka", "Rum", "Whiskey"];
+
+interface Drink {
+    idDrink: string;
+    strDrink: string;
+    strInstructions: string;
+    strDrinkThumb: string;
+}
 
 export default function Drinks (){
     const [selectedLiquor, setSelectedLiquor] = useState<string | null>(null)
     const [drinks, setDrinks] = useState([])
+    const [loading, setLoading] = useState(false);
 
     // fetch drinks when liquor has been selected
     async function handleLiquorClick(liquor: string) {
+        setLoading(true)
         setDrinks([]);
         setSelectedLiquor(liquor);
         console.log("Liquor: " + liquor);
@@ -20,10 +25,18 @@ export default function Drinks (){
         try {
             const response = await fetch(`http://localhost:8080/api/drinks/by-ingredient?ingredient=${liquor}`)
             const data = await response.json();
+
+            setDrinks(data);
+            setLoading(false);
             console.log(data);
         } catch(e: unknown){
-
+            console.error('Unable to fetch drinks');
         }
+    }
+
+    function handleBack(){
+        setSelectedLiquor(null)
+        setDrinks([])
     }
 
     return (
@@ -40,6 +53,17 @@ export default function Drinks (){
                         </button>
                     ))}
                 </div>
+            )}
+            {selectedLiquor && (
+                <>
+                    <button onClick={handleBack}>
+                        Return to List
+                    </button>
+                    {loading && (
+                        <div className="loading">Loading drinks...</div>
+                    )}
+                </>
+
             )}
         </section>
     )
